4a23a64c95e56489b641a5b53a1d623e3b4df3eb
Sam <samuelmi@live.unc.edu>
2021-11-12 16:10:52 -0500

    Restructured and added a shared class library

--- BethanysPieShopApp/obj/Debug/net5.0/RazorDeclaration/BethanysPieShopApp/Pages/Index.razor.g.cs
+++ BethanysPieShopApp/obj/Debug/net5.0/RazorDeclaration/BethanysPieShopApp/Pages/Index.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace BlazorTest
+namespace BlazorTest.BethanysPieShopApp.Pages
 {
     #line hidden
     using System;
@@ -13,76 +13,77 @@ namespace BlazorTest
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 1 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 2 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 3 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 4 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 5 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 6 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 7 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 8 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 9 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using BlazorTest;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\samum\GitubRepository\BlazorTest\_Imports.razor"
+#line 10 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
 using BlazorTest.Shared;
 
 #line default
 #line hidden
 #nullable disable
-    public partial class App : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
+    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
---
+++ SharedClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SharedClassLibrary
+{
+    public class Class1
+    {
+    }
+}
