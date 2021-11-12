7c60735cbdc78a33d8c0633ae7843a1c4e02163c
Sam <samuelmi@live.unc.edu>
2021-11-12 16:21:48 -0500

    Added assets to shared project

--- BethanysPieShopApp/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ BethanysPieShopApp/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -12,6 +12,76 @@ namespace BlazorTest
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
+#nullable restore
+#line 1 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using System.Net.Http;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using System.Net.Http.Json;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.AspNetCore.Components.Forms;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.AspNetCore.Components.Routing;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 7 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 8 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using Microsoft.JSInterop;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 9 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using BlazorTest;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 10 "c:\Users\samum\GitubRepository\BlazorTest\BethanysPieShopApp\_Imports.razor"
+using BlazorTest.Shared;
+
+#line default
+#line hidden
+#nullable disable
     public partial class App : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
